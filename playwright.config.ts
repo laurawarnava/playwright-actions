98269c9db014194ea642d351b6c48f92dd91bda5
Laura Cristine Warnava <laurawwarnava@gmail.com>
2025-12-12 16:59:27 -0300

    correção do cenário de login com usuário obrigatório e inclusão da url base

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,5 @@
+// playwright.config.ts
+
 import { defineConfig, devices } from '@playwright/test';
 
 /**
@@ -13,6 +15,9 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './e2e',
+
+  timeout: 90 * 1000, // 90 segundos
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -29,7 +34,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    navigationTimeout: 60 * 1000, // 60000ms
+    actionTimeout: 15 * 1000, // 15000ms
     trace: 'on-first-retry',
+    baseURL: 'https://loginxp.vercel.app'
   },
 
   /* Configure projects for major browsers */
@@ -39,41 +47,41 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //  name: 'firefox',
+    //  use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //  name: 'webkit',
+    //  use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
+    //  name: 'Mobile Chrome',
+    //  use: { ...devices['Pixel 5'] },
     // },
     // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
+    //  name: 'Mobile Safari',
+    //  use: { ...devices['iPhone 12'] },
     // },
 
     /* Test against branded browsers. */
     // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    //  name: 'Microsoft Edge',
+    //  use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
     // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    //  name: 'Google Chrome',
+    //  use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
+  //  command: 'npm run start',
+  //  url: 'http://localhost:3000',
+  //  reuseExistingServer: !process.env.CI,
   // },
 });
